e52e4438c8b38ea1f60c9bcbabc670de8e408d20
Ester <esterfania@users.noreply.github.com>
2020-08-06 20:09:41 -0300

    Adiciona atributo opcional de escape no template

--- app/ts/views/View.ts
+++ app/ts/views/View.ts
@@ -1,12 +1,16 @@
 export abstract class View<T> {
     private _elemento: JQuery;
-
-    constructor(seletor: string) {
+    private _escape: boolean;
+    constructor(seletor: string, escape?: boolean) {
         this._elemento = $(seletor);
+        this._escape = escape;
     }
 
     update(model: T) {
-        this._elemento.html(this.template(model));
+        let template = this.template(model);
+        if(this._escape)
+            template = template.replace(/<script>[\s\S]*?<\/script>/g, '')
+        this._elemento.html(template);
     }
 
     abstract template(model: T): string;
