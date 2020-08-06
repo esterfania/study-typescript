c86c57c880a080e4a0b74cda7372b48e954fa65c
Ester <esterfania@users.noreply.github.com>
2020-08-06 20:26:19 -0300

    Adiciona strictNullChecks

--- app/ts/views/View.ts
+++ app/ts/views/View.ts
@@ -1,7 +1,7 @@
 export abstract class View<T> {
     private _elemento: JQuery;
     private _escape: boolean;
-    constructor(seletor: string, escape?: boolean) {
+    constructor(seletor: string, escape: boolean = false) {
         this._elemento = $(seletor);
         this._escape = escape;
     }
