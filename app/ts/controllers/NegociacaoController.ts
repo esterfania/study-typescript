941900975ec99123026df94481385d3c1a91e960
Ester <esterfania@users.noreply.github.com>
2020-08-07 19:46:39 -0300

    Inclui throttle no adiciona -NegociacaoController

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -18,12 +18,13 @@ export class NegociacaoController {
     private _negociacoesView = new NegociacoesView("#negociacoesView", true);
     private _mensagemView = new MensagemView("#mensagemView");
     private _negociacaoParcialService = new NegociacaoParcialService();
+    
     constructor() {
         this._negociacoesView.update(this._negociacoes);
     }
 
-    adiciona(event: Event): void {
-        event.preventDefault();
+    @throttle()
+    adiciona(): void {
 
         let data = new Date(this._inputData.val().replace("/-/g", ","))
         if (!this._validaDiaUtil(data)) {
@@ -49,25 +50,6 @@ export class NegociacaoController {
 
     @throttle()
     importaDados() {
-
-        function isOk(res: Response) {
-            if (res.ok) {
-                return res;
-            } else {
-                throw new Error(res.statusText)
-            }
-        }
-        // fetch('http://localhost:8080/dados')
-        // .then(res => isOk(res))
-        // .then(res => res.json())
-        // .then((
-        //     dados: NegociacaoParcial[]) => {
-        //     dados
-        //         .map(dado => new Negociacao(new Date(), dado.vezes, dado.montante))
-        //         .forEach(negociacao => this._negociacoes.adiciona(negociacao));
-        //     this._negociacoesView.update(this._negociacoes);
-        // })
-        // .catch(err => console.log(err.message));       
 
         this._negociacaoParcialService
             .negociacoesParciais
