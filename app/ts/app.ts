4592ee063505660274cf0133541173f7a7637ad4
Ester <esterfania@users.noreply.github.com>
2020-08-07 19:36:30 -0300

    Adiciona função no app.ts para importar dados.

--- app/ts/app.ts
+++ app/ts/app.ts
@@ -3,3 +3,6 @@ import { NegociacaoController } from './controllers/NegociacaoController';
 const controller = new NegociacaoController();
 $(".form")
     .submit(controller.adiciona.bind(controller));
+
+$("#importa")
+    .click(controller.importaDados.bind(controller));
