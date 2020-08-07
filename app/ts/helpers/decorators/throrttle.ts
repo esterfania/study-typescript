b6045f34e7adb503317f4dbe4fe44b4d07a38574
Ester <esterfania@users.noreply.github.com>
2020-08-07 19:45:05 -0300

    Adiciona no decorator throttle o preventDefault

--- app/ts/helpers/decorators/throrttle.ts
+++ app/ts/helpers/decorators/throrttle.ts
@@ -1,11 +1,13 @@
 export function throttle(milisegundos = 500) {
 
     return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
-        
+
         const metodoOriginal = descriptor.value;
         let timer = 0;
-        
+
         descriptor.value = function (...args: any[]) {
+            if (event)
+                event.preventDefault()
 
             clearInterval(timer)
 
